602b0307d18843788685ffc9f4b91ead7cda5777
Ahmed <mr.ahmed_samer@yahoo.com>
2019-02-24 16:05:50 +0200

    add 404 page and test

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -16,4 +16,13 @@ class DefaultControllerTest extends WebTestCase
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
     }
+
+
+    public function test404Page()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/_error/404.html');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertContains('PAGE NOT FOUND.', $crawler->filter('.error-divider h2')->text());
+    }
 }
